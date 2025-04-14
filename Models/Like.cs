da51693e7b5c0589c68725e3e5c6460a80cdfbde
John <57923575+JohnsHub@users.noreply.github.com>
2025-04-14 01:17:45 +0100

    Update Like.cs
    Added navigation property

--- Models/Like.cs
+++ Models/Like.cs
@@ -9,7 +9,7 @@
 
         // Navigation properties
         public Post Post { get; set; }
-        // public User User { get; set; } // if using a user model
+        public User User { get; set; }
     }
 
 }
