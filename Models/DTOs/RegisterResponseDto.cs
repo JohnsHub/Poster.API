2e1ea89e73ca1386cbde6cd2f9e628486cca0f56
John <johncooperwill2@gmail.com>
2025-04-23 10:38:00 +0100

    Added Unit test

---
+++ Models/DTOs/RegisterResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Poster.API.Models.DTOs
+{
+    public class RegisterResponseDto
+    {
+        public string Id { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+    }
+}
